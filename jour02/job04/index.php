96e6be08e5d422a59608cb0705ee41ef642ec468
samuel-joly <samuel-joly@laplateforme.io>
2020-03-10 20:06:58 +0100

    End of job04

---
+++ jour02/job04/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+	<head>
+		<title>Runtrack3-jour2</title>
+	</head>
+	
+	<body>
+		<textarea id="keylogger" cols="40" rows="1"></textarea>
+		<script src="script.js"></script>
+	</body>
+	
+	
+	<footer>
+	
+	</footer>
+
+</html>
---
+++ jour02/job04/script.js
@@ -0,0 +1,4 @@
+document.addEventListener("keydown", function(e) {
+	text = document.getElementById("keylogger");
+	text.value += e.key;
+})
