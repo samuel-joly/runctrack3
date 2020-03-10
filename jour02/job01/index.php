14fd681a5e29766ac625bf152e048039fd0948a7
samuel-joly <samuel-joly@laplateforme.io>
2020-03-10 19:33:03 +0100

    Correct the button onclick function

--- jour02/job01/index.php
+++ jour02/job01/index.php
@@ -2,14 +2,14 @@
 
 <html lang="fr">
 	<head>
-		<title>Runtrack3-jour1</title>
+		<title>Runtrack3-jour2</title>
 		<script src="script.js">
 		</script>
 	</head>
 	
 	<body>
 		<article id="citation">La vie a beaucoup plus d’imagination que nous.</article>
-		<button id="button" onclick="citation()">Citation</button>
+		<button id="button">Citation</button>
 	</body>
 	
 	
--- jour02/job01/script.js
+++ jour02/job01/script.js
@@ -2,3 +2,5 @@ function citation()
 {
 	console.log(document.getElementById("citation").innerText);	
 }
+
+document.getElementById("button").onclick = citation();
