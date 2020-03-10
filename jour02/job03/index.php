074f67b4e04d7adbfae4015e728501804a559061
samuel-joly <samuel-joly@laplateforme.io>
2020-03-10 19:44:34 +0100

    End of job03

---
+++ jour02/job03/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+
+<html lang="fr">
+	<head>
+		<title>Runtrack3-jour2</title>
+	</head>
+	
+	<body>
+		<button id="button">Citation</button>
+		<article id="compteur">0</article>
+		<script src="script.js"></script>
+	</body>
+	
+	
+	<footer>
+	
+	</footer>
+
+</html>
---
+++ jour02/job03/script.js
@@ -0,0 +1,8 @@
+function addone()
+{
+	compteur = document.getElementById("compteur");
+	value = Number(compteur.innerHTML) + 1;
+	compteur.innerHTML=value;
+}
+
+document.getElementById("button").onclick = addone;
