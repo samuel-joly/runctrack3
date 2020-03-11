04c2cf50240fa0516a23a636446c9072c4b28eaf
samuel-joly <samuel-joly@laplateforme.io>
2020-03-11 20:36:27 +0100

    End of basic taquin gamemodel

--- jour03/job03/index.php
+++ jour03/job03/index.php
@@ -7,7 +7,7 @@
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 	</head>
 
-	<body style="Display:flex;flex-wrap:wrap;max-height:450px;max-width:500px;">
+	<body style="display:flex;flex-wrap:wrap;max-height:450px;max-width:500px;">
 		
 		
 		<script src="script.js"></script>
--- jour03/job03/script.js
+++ jour03/job03/script.js
@@ -1,18 +1,79 @@
-for(i=0;i<8;i++)
+function getPos(element)
 {
-	$("body").append("<div class='imgbox box"+i+"'></div>");
-	$(".box"+i).css("background-image","url(plateforme.png)");
-	var width = i*150;
-	if(i%3==0)
+	var enfants = $("body").children();
+	for(i=1;i<enfants.length;i++)
 	{
-		var height = i*150
+		if($(enfants[i]).attr("id") == element.attr("id"))
+		{
+			// console.log(i, element);
+			return i;
+		}
 	}
-	$(".box"+i).css("background-size",width+"px "+height+"px");
-	
 }
 
-$(".imgbox").css("width","150px");
-$(".imgbox").css("height","150px");
-$(".imgbox").css("margin","5px");
+var width = 0;
+var height = 0;
+
+for(i=0;i<8;i++)
+{
+	$("body").append("<div class='imgbox' id='box"+i+"'></div>");
+	$("#box"+i).css("background-image","url(plateforme.png)");
+	$("#box"+i).css("background-size","443px 443px");
+	$("#box"+i).css("background-position",width+"% "+height+"%");
+	
+	width += 50;
+	if(i>=2)
+	{
+		if(width == 150)
+		{
+			width = 0;			
+		}
+		if(i >=5)
+		{
+			height = 100;
+		}
+		else
+		{
+			height = 50;
+		}
+	}
+}
+$("body").append("<div class='imgbox' id='emptyBox'></div>");
+
+$(".imgbox").css("width","143px");
+$(".imgbox").css("height","143px");
+$(".imgbox").css("margin","1%");
 $(".imgbox").css("border","2px solid black");
 
+for(i=0;i<8;i++)
+{
+	var rand = Math.floor(Math.random()*8);
+	$("#box"+i).after($("#box"+rand));
+}
+
+$(".imgbox").click(function() {
+	if($(this).next().attr("id")=="emptyBox" && getPos($(this))%3 != 0)
+	{
+		
+		($(this)).insertAfter($("#emptyBox"));						
+		
+		
+	}
+	else if($(this).prev().attr("id")=="emptyBox")
+	{
+		($(this)).insertBefore($("#emptyBox"));
+	}
+	// else if()
+	else if($(this).next().next().next().attr("id")=="emptyBox")
+	{
+		var before = $(this).prev();
+		$(this).insertAfter($("#emptyBox"));
+		$("#emptyBox").insertAfter(before);
+	}
+	else if($(this).prev().prev().prev().attr("id")=="emptyBox")
+	{
+		var before = $(this).prev();
+		$(this).insertBefore($("#emptyBox"));
+		$("#emptyBox").insertAfter(before);
+	}
+})
