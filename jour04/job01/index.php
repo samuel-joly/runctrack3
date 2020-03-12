486c9a4a588ae9dcde0e15c24c9de0d60dee3022
samuel-joly <samuel-joly@laplateforme.io>
2020-03-12 10:25:12 +0100

    Start of jour04

---
+++ jour04/job01/index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+
+
+<html>
+	<head>
+		<title>Jour04</title>
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+	</head>
+
+	<body>
+		<header>
+		</header>
+
+		<main>	
+			<button id="button">Ecrire</button>
+		</main>
+
+		<footer>
+		</footer>
+		
+		<script src="script.js"></script>
+	</body>
+</html>
---
+++ jour04/job01/script.js
@@ -0,0 +1,10 @@
+$("button").click(function (){
+	$.ajax({
+		type: "GET",
+		url: "expression.txt",
+		success:function(data)
+		{
+			$("body").append(data);		
+		}
+	});
+});
