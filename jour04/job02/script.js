07c69ea0520fb3035c45ec07ab59ecb070962c3f
samuel-joly <samuel-joly@laplateforme.io>
2020-03-12 10:40:18 +0100

    End of job02

---
+++ jour04/job02/script.js
@@ -0,0 +1,12 @@
+function jsonValueKey(json_txt, key)
+{
+	let value = JSON.parse(json_txt);
+	return value[key];
+}
+
+
+
+
+var json = '{"name":"La Plateforme_", "address":"8 rue d\'hozier", "city":"Marseille", "nb_staff":"11", "creation":"2019"}';
+
+console.log(jsonValueKey(json, "adress"));
